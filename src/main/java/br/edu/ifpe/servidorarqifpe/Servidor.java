254988011cd9fd888b0ae24bcb13ebf37349fc6a
Guilherme Silva <guilherme.dna06@gmail.com>
2017-09-10 10:10:53 -0300

    Adicionada Threads para o servidor se tornar bidirecional

--- src/main/java/br/edu/ifpe/servidorarqifpe/Servidor.java
+++ src/main/java/br/edu/ifpe/servidorarqifpe/Servidor.java
@@ -1,17 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.ifpe.servidorarqifpe;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
  *
- * @author gon
+ * @author Guilherme
  */
 public class Servidor {
 
@@ -23,19 +22,53 @@ public class Servidor {
             Socket cliente = servidor.accept();
             System.out.println("Nova conexão com o cliente "
                     + cliente.getInetAddress().getHostAddress());
+            Thread t1 = new Thread(new Runnable() {
 
-            Scanner s = new Scanner(cliente.getInputStream());
-            while (s.hasNextLine()) {
-                String escrita = s.nextLine();
-                if (escrita.equals("sair")) {
-                    break;
+                public void run() {
+                    try {
+                        Scanner teclado = new Scanner(cliente.getInputStream());
+
+                        while (teclado.hasNextLine()) {
+                            String escrita = teclado.nextLine();
+                            if (escrita.equals("sair")) {
+                                System.exit(0);
+                            }
+                            System.out.println(escrita);
+                        }
+
+                        teclado.close();
+
+                        servidor.close();
+
+                        cliente.close();
+                    } catch (Exception e) {
+
+                    }
                 }
-                System.out.println(escrita);
-            }
+            });
+            t1.start();
 
-            s.close();
-            servidor.close();
-            cliente.close();
+            Thread t2 = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        PrintStream saida = new PrintStream(cliente.getOutputStream());
+                        Scanner teclado = new Scanner(System.in);
+
+                        while (teclado.hasNextLine()) {
+
+                            saida.println(teclado.nextLine());
+                        }
+
+                        servidor.close();
+                        cliente.close();
+                        teclado.close();
+                    } catch (Exception e) {
+
+                    }
+                }
+            });
+            t2.start();
+
         }
     }
 
